3df1a7aca2d961fa37d204d5160b75959ac0d333
Carlos Santos <62728109+carlosantoss@users.noreply.github.com>
2020-06-25 11:08:19 -0300

    Add files via upload

---
+++ scripts/cenas/telaInicial.js
@@ -0,0 +1,27 @@
+class TelaInicial {
+  constructor() {}
+
+  draw() {
+    this._imagemDeFundo();
+    this._texto();
+    this._botao();
+  }
+
+  _imagemDeFundo() {
+    image(imagemTelaInicial, 0, 0, width, height);
+  }
+
+  _texto() {
+    textFont(fonteTelaInicial);
+    textAlign(CENTER);
+    textSize(50);
+    text('As aventuras de', width / 2, height / 3);
+    textSize(150);
+    text('Hipstar', width / 2, height / 5 * 3);
+  }
+
+  _botao() {
+    botaoGerenciador.y = height / 7 * 5;
+    botaoGerenciador.draw();
+  }
+}
--- scripts/jogo/personagem.js
+++ scripts/jogo/personagem.js
@@ -1,34 +1,34 @@
-class Personagem extends Animacao{
-  constructor(matriz,imagem,x,variacaoY,largura,altura,larguraSprite,alturaSprite){
-    super(matriz,imagem,x,variacaoY,largura,altura,larguraSprite,alturaSprite)
-    
+class Personagem extends Animacao {
+  constructor(matriz, imagem, x, variacaoY, largura, altura, larguraSprite, alturaSprite) {
+    super(matriz, imagem, x, variacaoY, largura, altura, larguraSprite, alturaSprite)
+
     this.variacaoY = variacaoY
     this.yInicial = height - this.altura - this.variacaoY
     this.y = this.yInicial
-    
+
     this.velocidadeDoPulo = 0
     this.gravidade = 6
     this.alturaDoPulo = -50
     this.pulos = 0
-    }
-  
+  }
+
   pula() {
-    if(this.pulos < 3) {
-    this.velocidadeDoPulo = this.alturaDoPulo
-    this.pulos++
+    if (this.pulos < 3) {
+      this.velocidadeDoPulo = this.alturaDoPulo
+      this.pulos++
     }
   }
-  
+
   aplicaGravidade() {
     this.y = this.y + this.velocidadeDoPulo
     this.velocidadeDoPulo = this.velocidadeDoPulo + this.gravidade
-    
-    if(this.y > this.yInicial){
+
+    if (this.y > this.yInicial) {
       this.y = this.yInicial
       this.pulos = 0
     }
   }
-  
+
   estaColidindo(inimigo) {
     const precisao = .7
     const colisao = collideRectRect(
@@ -41,7 +41,7 @@ class Personagem extends Animacao{
       inimigo.largura * precisao,
       inimigo.altura * precisao
     );
-    
+
     return colisao;
   }
 }
